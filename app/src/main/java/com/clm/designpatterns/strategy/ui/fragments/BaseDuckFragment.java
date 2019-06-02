3002fd7947b96fd48a2f90494371dc5b8e1ffbff
Christopher McKinney <123topher456@gmail.com>
2019-06-02 12:47:25 -0500

    Added ui and ui duck behavior for duck fragments.

---
+++ app/src/main/java/com/clm/designpatterns/strategy/ui/fragments/BaseDuckFragment.java
@@ -0,0 +1,6 @@
+package com.clm.designpatterns.strategy.ui.fragments;
+
+import com.clm.designpatterns.base.BaseFragment;
+
+public abstract class BaseDuckFragment extends BaseFragment {
+}
---
+++ app/src/main/java/com/clm/designpatterns/util/MediaPlayerUtil.java
@@ -0,0 +1,4 @@
+package com.clm.designpatterns.util;
+
+public class MediaPlayerUtil {
+}
